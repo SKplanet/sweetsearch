4d8c7377ea60ca075aedd6c1fd83fc1f56888142
윤지수(Jisu Youn)/CTO/SKP <jisu.youn@sk.com>
2016-02-29 17:19:49 +0900

    Add. jasmine javascript test framework

---
+++ spec/test.js
@@ -0,0 +1,11 @@
+describe("A suite", function() {
+
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+
+  it("contains spec with an expectation", function() {
+    expect(false).toBe(true);
+  });
+
+});
